c72a74a8a112deb98e0c5d4bda1d3796e6b1e8c3
Jon Ball <91580027+j-a-ball@users.noreply.github.com>
2023-10-02 19:18:24 -0700

    Delete examples directory

--- examples/quantize-7b.sh
+++
@@ -1,9 +0,0 @@
-#!/ usr/ bin / env bash
-git clone https://github.com/ggerganov/llama.cpp
-cd ./llama.cpp
-cd ./models
-mkdir 7B 
-python3 convert.py --outfile models/7B/base-f16.bin --outtype f16 ../CodeLlama-7b
-make
-./quantize ./models/7B/code-base-f16.bin ./models/7B/code-base-q4_0.bin q4_0
-./main -m ./models/7B/code-base-q4_0.bin -n 128
