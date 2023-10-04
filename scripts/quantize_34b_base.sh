0eecf247002511fcb6d15c2018bb755cf3aeb15c
Jon Ball <91580027+j-a-ball@users.noreply.github.com>
2023-10-04 14:24:15 -0700

    Update quantize_34b_base.sh

--- scripts/quantize_34b_base.sh
+++ scripts/quantize_34b_base.sh
@@ -10,4 +10,4 @@ mkdir ./models/34b
 # Convert model -- PATH TO YOUR DOWNLOADED LLAMA WEIGHTS HERE
 python3 convert.py ../CodeLlama-34b/ --outfile models/34b/base-f16.gguf
 # Quantize the model (run ./quantize --help for more info)
-./quantize ../CodeLlama-7b/ggml-model-f16.gguf ./models/7B/code-base-Q5_K.bin Q5_K
+./quantize ../CodeLlama-7b/ggml-model-f16.gguf ./models/34b/code-base-Q5_K.bin Q5_K
